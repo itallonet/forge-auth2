340ee4f503353b1a6368a334f639d0be95479f09
itallonet <italloriancs@gmail.com>
2024-07-29 22:58:14 -0300

    feature: entity creation sucess and validated

---
+++ app/src/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class Entity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<Entity {self.email}>'
